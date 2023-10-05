643ad1a3db36c27845f712951a29882a1bc0d258
meaxzy <olayinkawu@gmail.com>
2023-10-05 15:33:35 -0700

    Argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if num_args == 0:
         print("0 argument(s).")
     else:
-        if num_args > 1:
+        if num_args != 0:
             print("{} argument(s): ".format(num_args), end='')
         for i in range(1, num_args + 1):
             print("{}: {}".format(i, sys.arg[i]))
