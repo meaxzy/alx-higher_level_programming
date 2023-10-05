318a0679804923101fc1f032bee55bea504d9e3f
meaxzy <olayinkawu@gmail.com>
2023-10-05 15:20:17 -0700

    Argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,13 +2,13 @@
 if __name__ == "__main__":
     import sys
     args = sys.argv
-    num_args = len(args)
+    num_args = len(args) - 1
     if num_args == 1:
         print("0 argument(s).")
     else:
         if num_args == 2:
             print("1 argument:")
         else:
-            print("{} argument(s): ".format(num_args - 1))
-        for i in range(1, num_args):
+            print("{} argument(s): ".format(num_args))
+        for i in range(1, num_args + 1):
             print("{}: {}".format(i, sys.arg[i]))
