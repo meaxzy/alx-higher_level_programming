245b088c320fa4e45261efc956a02ef664c37904
meaxzy <olayinkawu@gmail.com>
2023-10-05 05:12:44 -0700

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,6 +3,5 @@ def add(a, b):
     a = 1
     b = 2
 
-result = add_0(a, b)
-
-print("{} + {} = {}".format(a, b, result), end='')
+    result = add_0(a, b)
+    print("{} + {} = {}".format(a, b, result), end='')
