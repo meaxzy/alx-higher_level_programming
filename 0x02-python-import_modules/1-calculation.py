cd26db5534d441ce6eb32e9570fae5cc88196701
meaxzy <olayinkawu@gmail.com>
2023-10-05 06:08:31 -0700

    cal

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import calculator_1 as cal
+    a = 10
+    b = 5
+    c = cal.add(a, b)
+    d = cal.sub(a, b)
+    e = cal.mul(a, b)
+    f = cal.div(a, b)
+    print("{} + {} = {}".format(a,b,c))
+    print("{} - {} = {}".format(a,b,d))
+    print("{} * {} = {}".format(a,b,e))
+    print("{} / {} = {}".format(a,b,f))
