7eb2649987e419e30a38f51876f67a18bbad911d
meaxzy <olayinkawu@gmail.com>
2023-10-07 15:32:27 -0700

    Replacing new element

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or id >= len(my_list):
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
