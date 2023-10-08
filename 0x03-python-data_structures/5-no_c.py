90470bbe95bfcc0ecfe2aa6a2bac27ecc01fae36
meaxzy <olayinkawu@gmail.com>
2023-10-08 14:25:07 -0700

    No letter c and C

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in range(len(my_string)):
-        if my_string[i] == 'c' or my_string[i] == 'C':
-            continue
-        print("{:s}".format(my_string[i]), end='')
+    string = ""
+    for i in my_string:
+        if i != 'c' or i != 'C':
+            string += i
+            return string
