4cb271128e2077fc502e306ef31e286125c0d25b
meaxzy <olayinkawu@gmail.com>
2023-10-10 08:14:54 -0700

    Divisible by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    Value = []
+    for num in range(len(my_list)):
+        if num % 2 == 0:
+            return True
+        else:
+            return False
