3cac9eeafdac8bb2b40b9529c7b0bea33c1c5cd7
meaxzy <olayinkawu@gmail.com>
2023-10-11 13:27:38 -0700

    Task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 def search_replace(my_list, search, replace):
     def search_replace(my_list, search, replace):
     new_list = []
-    for item in range(len(my_list)):
+    for item in my_list:
         if item == search:
             item == replace
         else:
