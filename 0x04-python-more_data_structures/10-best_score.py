761415690df3cc51c631724d326fe0f2f8f42934
meaxzy <olayinkawu@gmail.com>
2023-10-12 07:37:15 -0700

    Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,8 +2,8 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    highest_score = a_dictionary[0]
-    for x in a_dictionary:
-        if a_dictionary[x] > highest_score:
-            highest_score = a_dictionary[x]
+    highest_score = None
+    for key in a_dictionary:
+        if highest_score is None or a_dictionary[key] > highest_score:
+            highest_score = a_dictionary[key]
     return highest_score
