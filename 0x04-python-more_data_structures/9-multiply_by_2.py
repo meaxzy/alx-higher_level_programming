cb346e99ad247a3654d9e5e28a343661709e8497
meaxzy <olayinkawu@gmail.com>
2023-10-12 06:55:41 -0700

    Task 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    result = {}
     for key in a_dictionary:
-    return a_dictionary[key ** 2]
+        result[key] = value ** 2
+    return result
