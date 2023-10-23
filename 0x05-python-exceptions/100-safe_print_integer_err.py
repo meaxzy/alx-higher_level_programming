8ea34a48ba1c5d71098e81d10665c9d5f7188a17
meaxzy <olayinkawu@gmail.com>
2023-10-23 16:20:27 -0700

    advance

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,8 +6,8 @@ def safe_print_integer_err(value):
     try:
         integer_value = int(value)
         print("{:d}".format(integer_value))
-        print()
         return True
+    print()
     except (ValueError, TypeError):
         print(error_message, file=sys.stderr)
         return False
