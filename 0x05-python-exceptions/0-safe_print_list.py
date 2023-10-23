3cd9f1b7b8fc58e9bb8ea7cca6c622ecb940101d
meaxzy <olayinkawu@gmail.com>
2023-10-23 12:58:37 -0700

    print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,6 +7,6 @@ def safe_print_list(my_list=[], x=0):
         except IndexError:
             break
         print_integer = print_integer + 1
-        print("")
+    print("")
 
     return (print_integer)
