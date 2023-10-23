7cf91d305066bf4138c0f5a53f670e2a59c4ec17
meaxzy <olayinkawu@gmail.com>
2023-10-23 14:25:01 -0700

    Division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except (ZeroDivisionError, TypeError):
+    except (ZeroDivisionError, ValueError):
         print("Division by zero is not allowed.")
         result = None
     finally:
