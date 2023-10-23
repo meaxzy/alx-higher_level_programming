3c91fc2fcdc37f48d993acdfd40bf74ab790e04e
meaxzy <olayinkawu@gmail.com>
2023-10-23 16:11:45 -0700

    advance

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,9 +3,9 @@ import sys
 
 def safe_print_integer_err(value):
     try:
+        integer_value = int(value)
         print("{:d}".format(integer_value))
         return True
     except (ValueError, TypeError):
-        error_message = "Exception: Value is not an integer"
         print(error_message, file=sys.stderr)
         return False
