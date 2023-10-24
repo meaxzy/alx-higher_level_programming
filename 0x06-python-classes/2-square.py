b1ced8a8d53a42146d9affe70717dfbe4428ee91
meaxzy <olayinkawu@gmail.com>
2023-10-24 12:06:54 -0700

    Update

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
+""" defines a class named Square """
+
+
 class Square:
     """ Defines a class Square """
-    def __init__(self, size=0):
+   
+   def __init__(self, size=0):
         """ Raise an error if not an integer """
+        
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         """ Raise an error if less than zero """
-        elif size < 0:
+        
+    elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
-    def area(self):
-        return self.__size ** 2
