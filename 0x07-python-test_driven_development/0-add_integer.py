24fb76f4f92de717d4a6b257c550a210dff73369
meaxzy <olayinkawu@gmail.com>
2023-10-29 13:17:19 -0700

    add task 1

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    if not isinstance(a, (int, float)):
+        raise TypeError(a must be an integer)
+    else
+    int (a)
+    if not isinstance(b, (int, float)):
+        raise TypeError(b must be an integer)
+    else
+    int (b)
+    return int(a + b)
+
