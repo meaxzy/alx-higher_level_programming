f6867beefe961af1edaadf3f199c91f430909456
meaxzy <olayinkawu@gmail.com>
2023-10-30 10:14:12 -0700

    add first task

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
+
+
+ """A class that defines a rectangle."""
 class Rectangle:
+ """Empty  class that defines a rectangle."""
 
-
-    """A class that defines a rectangle."""
-    def __init__(self, width, height):
-        """Initialize a rectangle with the given width and height.
-
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-        """
-        self.width = width
-        self.height = height
+ pass
