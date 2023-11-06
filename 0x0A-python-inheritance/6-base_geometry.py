9cb25bb97508c674c8f0cb285b11b8f17cc13a5e
meaxzy <olayinkawu@gmail.com>
2023-11-06 13:14:59 -0800

    task 7

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Define class called BaseGeometry """
+
+
+class BaseGeometry:
+    """ Defining class with atrributes and methods """
+
+    def area(self):
+
+        """
+        Calculate the area (not implemented).
+
+        """
+    try:
+        print(BaseGeometry().area())
+    except Exception("area() is not implemented")
