3f8ea4719b142100827e97986dd461ea498c47b7
meaxzy <olayinkawu@gmail.com>
2023-11-08 13:20:21 -0800

    add tasks

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python3
-""" Check if the object is an instance of a class that inherited """
+"""Module containing inherits_from method"""
 
 
 def inherits_from(obj, a_class):
-
-    """
-    Check if an object is an instance of a class that inheritedform a class
-
-    Args:
-        obj: The object to check.
-        a_class: The class to compare inheritance with.
-
-    """
-    return isinstance(obj, a_class) and (type(obj) =! a_class)
+    """returns True if the object is an instance of a class that
+    inherited (directly or indirectly) from the specified class;
+    otherwise False"""
+    return isinstance(obj, a_class) and type(obj) != a_class
